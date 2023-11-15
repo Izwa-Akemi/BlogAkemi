317e99fc68f701369cf36205e5f8edf7dcee8fa2
Izwa-Akemi <sirokumaorange1129@gmail.com>
2023-11-15 10:13:23 +0900

    プロジェクトの作成

---
+++ BlogAkemi/src/main/java/blog/com/BlogAkemiApplication.java
@@ -0,0 +1,13 @@
+package blog.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAkemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAkemiApplication.class, args);
+	}
+
+}
